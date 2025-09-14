db1cf760c5a40a915749caa08c2448b8c758ed7d
chitresh99 <chitreshpoojary990@gmail.com>
2025-09-14 23:30:34 +0530

    chore: add basic boilerplate code

---
+++ main.py
@@ -0,0 +1,96 @@
+from fastapi import FastAPI
+from openai import OpenAI
+from dotenv import load_dotenv
+from pydantic import BaseModel
+import os
+
+load_dotenv()
+
+app = FastAPI()
+
+OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
+print(OPENROUTER_API_KEY)
+
+"""
+The requirements to build basic galileonix are
+ Understanding the Business Problem
+ Data Collection
+ Data Cleaning / Preprocessing
+ Data Exploration (EDA : Exploratory Data Analysis)
+ Data Visualization
+ Deriving Insights and Recommendations
+ Reporting
+"""
+
+
+@app.get("/")
+def root():
+    return {"message": "Welcome to DAK, this is Trixy"}
+
+
+class Problem(BaseModel):
+    statement: str
+
+
+@app.post("/business-problem")
+def business_problem(problem: Problem):
+    request = problem.statement
+
+    client = OpenAI(
+        base_url="https://openrouter.ai/api/v1",
+        api_key=OPENROUTER_API_KEY,
+    )
+
+    completion = client.chat.completions.create(
+        model="deepseek/deepseek-r1-0528-qwen3-8b:free",
+        messages=[
+            {
+                "role": "system",
+                "content": "You are an expert at understanding the business problem",
+            }
+        ]
+    )
+
+
+    print(completion.choices[0].message.content)
+
+
+@app.post("/data-collection")
+def data_collection():
+    # collect data from multiple source
+    # api basic hit to any endpoint
+    # connect to sql db's
+    # then put these together in a unfied source that is csv
+    pass
+
+
+@app.post("/data-cleaning")
+def data_cleaning():
+    # clean data using autoclean or i'll write a simple custom script
+    # first provide insights then use autoclean to clean the data
+    # return the final file
+    pass
+
+
+@app.post("/data-viz")
+def data_viz():
+    # get the info from the dataset
+    # then return the stats
+    # we'll viz on the frontend
+    pass
+
+
+@app.post("/assistant")
+def analytics():
+    # upload csv
+    # write a feedback engine
+    # build analytics
+    pass
+
+
+@app.post("/reporter")
+def reporter():
+    # csv -> get stats -> report
+    # connect to db run queries basic sql one's via ai
+    # get a detailed report
+    pass
